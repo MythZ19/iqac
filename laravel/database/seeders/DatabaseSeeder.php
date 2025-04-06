2fdc75a5f0b748e205d16e56ef20215208358985
Zuia <lalzuitluanga19@gmail.com>
2025-04-06 19:51:26 +0530

    updates

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -3,14 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
@@ -19,6 +15,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin User',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('admin'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('test'),
+            'role' => 'user',
         ]);
     }
 }

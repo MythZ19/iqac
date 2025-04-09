357b8078476990893c30699b43b95f0c37cd56ad
Zuia <lalzuitluanga19@gmail.com>
2025-04-09 22:56:58 +0530

    route update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (auth()->check()) {
-        return redirect('/dashboard');
-    }
     return redirect('/admin');
 })->name('home');
